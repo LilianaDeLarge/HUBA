79479749660c4d89053a9f7f007c8fa80bb3de51
Liliana DeLarge <LilianaDeLarge@kuku.ru>
2020-10-14 23:11:45 +0300

    Create Test_id.py

---
+++ Test_id.py
@@ -0,0 +1,16 @@
+id_veb={}
+
+def check_id():
+    user_id=1
+    while user_id!=0:
+        user_id=int(input("Id:"))
+        qwest=int(input("Page:"))
+        
+        if (user_id in id_veb):
+            print("Here")
+        else:
+            id_veb[user_id]=qwest
+        print(id_veb)
+    fun()
+def fun():
+    print("ok")
